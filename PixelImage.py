14e127405d83c1c74afc5976ff24ea6752c39ed0
Maxime Lenfant <maxime.lenfant@free.fr>
2026-05-12 16:23:16 +0200

    Refactor (main) Avancé sur le RGB2Smarties Ajout de nouvelles fonction

--- PixelImage.py
+++ PixelImage.py
@@ -4,6 +4,31 @@ import numpy as np
 from pathlib import Path
 from tkinter import filedialog
 import pickle as pck
+from math import sqrt
+import csv
+
+RGBMatTemp = (
+    ("Orange", [225, 101, 54]),
+    ("Rouge", [208, 46, 73]),
+    ("Jaune", [224, 209, 78]),
+    ("Mauve", [171, 147, 207]),
+    ("Rose", [222, 120, 182]),
+    ("Cyan", [119, 200, 227]),
+    ("Lime", [141, 196, 102]),
+    ("Marron", [133, 77, 76])
+)
+
+RGBMatTemp = (
+    ("Orange", [225, 101, 54]),
+    ("Rouge", [208, 46, 73]),
+    ("Jaune", [224, 209, 78]),
+    ("Mauve", [171, 147, 207]),
+    ("Rose", [222, 120, 182]),
+    ("Cyan", [119, 200, 227]),
+    ("Lime", [141, 196, 102]),
+    ("Marron", [133, 77, 76])
+)
+
 
 class PixelImage:
 	def __init__(self, img_path=None, pixel_size=(20, 20)):
@@ -32,26 +57,86 @@ class PixelImage:
 		else:
 			raise ValueError("Output image not generated yet.")
 		
-	def pixelisation(self):
+	def pixelisation(self, show = False, out = False) -> list:
+		'''
+		Pixelise the image, return a list of list of RGB values.
+		If show is True, display the input and output images.
+		If out is True, save the output image as "test_pixelated.jpg".
+
+		TO DO : add a parameter to choose the output path and name.
+		'''
 		height, width = self.img.shape[:2]
-		w, h = (15, 15)
+		w, h = self.pixel_size
+
 		temp = cv2.resize(self.img, (w, h), interpolation=cv2.INTER_LINEAR)
 
-		output = cv2.resize(temp, (width, height), interpolation=cv2.INTER_NEAREST)
+		OutVisu = cv2.resize(temp, (width, height), interpolation=cv2.INTER_NEAREST)
 
-		output2 = cv2.resize(temp, (w, h), interpolation=cv2.INTER_NEAREST)
+		OutUtil = cv2.resize(temp, (w, h), interpolation=cv2.INTER_NEAREST)
 
-		cv2.imshow('Input', self.img)
-		cv2.imshow('Output', output)
-		#cv2.imwrite("test_pixelated.jpg", output)
+		if show:
+			cv2.imshow('Input', self.img)
+			cv2.imshow('Output', OutVisu)
+
+		if out : cv2.imwrite("test_pixelated.jpg", OutVisu)
+
 		ImageMat = open('ImageMat.txt', 'wb')
-		pck.dump(output2.tolist(),ImageMat)
+		pck.dump(OutUtil.tolist(),ImageMat)
 
-		output.tolist()
-		cv2.waitKey(0)
-		cv2.destroyAllWindows()
+		OutVisu.tolist()
+		return OutUtil.tolist()
+
+	def nearest_colour(self, subjects, query ):
+		'''
+		Find the nearest colour in subjects to the query colour.
+		'''
+		indice = 100000
+		for CouleurSmarties in subjects :
+			s = 0
+			for i in range(3):
+				s += (CouleurSmarties[1][i] - query[i])**2
+			if indice > s :
+				indice = s
+				top = CouleurSmarties
+			#indice = min( subjects, key = lambda subject: sum( (s[1] - q) ** 2 for s, q in zip( tuple(CouleurSmarties), tuple(query) ) ) )
+		return CouleurSmarties
+	
+	def RGB2Smarties(self, RGBMat:list) ->list :
+		#Ouverture du fichier contenant les couleurs des smarties vers une liste
+		RGB_Smart_Mat = [] # Matrice des couleurs des Smarties en RGB	
+		Smarties_Mat_Select = [] # Matrice des smarties selectionnés
+		with open('couleurs.csv', newline='') as csvCoul :
+			reader = csv.reader(csvCoul, delimiter=';')
+			for row in reader :
+				RGB_Smart_Mat.append(RGBMatTemp[1])
+			#RGB_Smart_Mat = tuple(RGB_Smart_Mat)
+
+		#Conversion en cherchant la couleur de smarties la plus proche.
+		tempROW = []
+		for row in RGBMat:
+			for pixel in row:
+				tempROW.append(self.nearest_colour(RGB_Smart_Mat, pixel))
+			Smarties_Mat_Select.append(tempROW)
+			tempROW = []
+		return Smarties_Mat_Select
 
 
-poubelle = PixelImage(img_path="image/tilted.png")
-poubelle.load_image()
-poubelle.pixelisation()
+if True :
+	poubelle = PixelImage(img_path="image/tilted.png")
+	poubelle.load_image()
+	Mat = poubelle.pixelisation()
+	for i in range(len(Mat)):
+		print(Mat[i])
+		for j in range(len(Mat[i])):
+			if j >19 :
+				Mat[i].pop(i)
+
+	hexMat = poubelle.RGB2Smarties(Mat)
+	for i in range(len(hexMat)):
+		print(hexMat[i])
+	gen = np.array(hexMat,dtype=np.uint16)
+	cv2.imshow('i',gen)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
